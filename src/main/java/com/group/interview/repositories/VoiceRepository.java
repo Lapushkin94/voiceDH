82acd071f547eddba3b7c50549859b49617d2f62
Vladimir <vladimir.lapushkin@telekom.com>
2024-03-21 12:40:06 +0200

    crud impl created

--- src/main/java/com/group/interview/repositories/VoiceRepository.java
+++ src/main/java/com/group/interview/repositories/VoiceRepository.java
@@ -3,5 +3,5 @@ package com.group.interview.repositories;
 import com.group.interview.domain.PhoneNumber;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VoiceRepository extends CrudRepository<PhoneNumber, String> {
+public interface VoiceRepository extends CrudRepository<PhoneNumber, Long> {
 }
